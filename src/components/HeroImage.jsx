8db7e33df9874fa73547fe393affdc71ace57a61
sepriandani <sepriandanni@gmail.com>
2024-01-11 11:02:50 +0700

    make test pass

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -3,7 +3,7 @@ import LoginImage from '../image/login-image.svg'
 export default function HeroImage() {
   return (
     <div className="flex-1">
-      <div className="text-4xl mb-8">Discussion Forum</div>
+      <div className="text-4xl mb-8">Discussion Forum Website</div>
       <img className="" src={LoginImage} alt="login-image" />
     </div>
   )
