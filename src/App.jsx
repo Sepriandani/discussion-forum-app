0df1d00ab1c0dc6b1f1bcd16a1b694ec97ad9f0d
sepriandani <sepriandanni@gmail.com>
2024-01-11 08:58:54 +0700

    fix error deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { Route, Routes } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import ThreadsPage from './pages/ThreadsPage'
 import LeaderboardsPage from './pages/LeaderboardsPage'
